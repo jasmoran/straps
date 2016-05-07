96e978267f68c5350177cc5dc9bf0b27f15be03c
Jack Moran <jack@earth.co.nz>
2016-05-07 17:59:05 +1200

    Port the v9 implementation to v9

--- build.sh
+++ build.sh
@@ -45,7 +45,18 @@
 # gcc -m64 -o bin/v8 bin/v8.o &&
 # rm bin/v8.o &&
 
-./bin/v8 src/v9.v8 asm_out/v9.asm &&
-nasm -f elf64 -o bin/v9.o asm_out/v9.asm &&
-gcc -m64 -o bin/v9 bin/v9.o &&
-rm bin/v9.o
+# ./bin/v8 src/v9.v8 asm_out/v9.asm &&
+# nasm -f elf64 -o bin/v9.o asm_out/v9.asm &&
+# gcc -m64 -o bin/v9 bin/v9.o &&
+# rm bin/v9.o &&
+
+./bin/v9 src/v9.v9 asm_out/v99.asm &&
+nasm -f elf64 -o bin/v99.o asm_out/v99.asm &&
+gcc -m64 -o bin/v99 bin/v99.o &&
+rm bin/v99.o
+
+# ./bin/v99 src/v9.v9 asm_out/v99t.asm &&
+# nasm -f elf64 -o bin/v99t.o asm_out/v99t.asm &&
+# gcc -m64 -o bin/v99t bin/v99t.o &&
+# rm bin/v99t.o &&
+# diff asm_out/v99.asm asm_out/v99t.asm | head
