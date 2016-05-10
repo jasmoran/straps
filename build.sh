25c34e1cc50cb976fc7d5908b652ddc886334f4f
Jack Moran <jack@earth.co.nz>
2016-05-10 22:05:57 +1200

    Complete AST
    Now to write a whole bunch of visitors...

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-./bin/v9 src/v9.v9 asm_out/v99.asm &&
-nasm -f elf64 -o bin/v99.o asm_out/v99.asm &&
-gcc -m64 -o bin/v99 bin/v99.o &&
-rm bin/v99.o
+./bin/v9 src/v10.v9 asm_out/v10.asm &&
+nasm -f elf64 -o bin/v10.o asm_out/v10.asm &&
+gcc -m64 -o bin/v10 bin/v10.o &&
+rm bin/v10.o
 
 # ./bin/v99 src/v9.v9 asm_out/v99t.asm &&
 # nasm -f elf64 -o bin/v99t.o asm_out/v99t.asm &&
