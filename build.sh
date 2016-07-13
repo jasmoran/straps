652463b17402fc8807ac8a7fa59f585cbae8f1e6
Jack Moran <jack@earth.co.nz>
2016-07-13 21:50:08 +1200

    Created unit testing framework. Created tests for all String methods

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@
 nasm -f elf64 -o bin/v10.o asm_out/v10.asm &&
 gcc -m64 -o bin/v10 bin/v10.o &&
 rm bin/v10.o
+rm bin/v10.o &&
 
-# ./bin/v99 src/v9.v9 asm_out/v99t.asm &&
-# nasm -f elf64 -o bin/v99t.o asm_out/v99t.asm &&
-# gcc -m64 -o bin/v99t bin/v99t.o &&
-# rm bin/v99t.o &&
-# diff asm_out/v99.asm asm_out/v99t.asm | head
+./run_test -q test/**/*
