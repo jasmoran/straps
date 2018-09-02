4fcc254ad144ac99252b4b51bccdaa5a58f1475e
Jack Moran <jack@earth.co.nz>
2018-09-02 17:35:52 +1200

    Remove unused scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./bin/v1010 src/language/v10.v10 asm_out/straps.asm &&
-nasm -f elf64 -o bin/straps.o asm_out/straps.asm &&
-ld -m elf_x86_64 -o bin/straps bin/straps.o &&
-rm bin/straps.o &&
-./run_test -q
