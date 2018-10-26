24a68ca2548c257a6651a91dc6b2d941d44968d1
Jack Moran <jack@earth.co.nz>
2018-10-26 20:16:17 +1300

    Fix ups

--- package_sources.rb
+++ package_sources.rb
@@ -2,7 +2,7 @@ require 'json'
 
 File.open('fs.js', 'w') do |f|
   f.write('const files = ')
-  f.write(JSON.generate(Dir['src/**/*.v10'].each_with_object({}) do |path, ob|
+  f.write(JSON.generate(Dir['src/**/*.{v10,generic}'].each_with_object({}) do |path, ob|
     next unless File.file? path
     ob[path] = File.read(path)
   end))
